d1e35c65e6c37f7a901d72ad8b3a7225b02b44fa
Aaryan Thakur <princeaaryan06@gmail.com>
2025-05-31 12:34:24 +0545

    first commit on this branch

---
+++ class.py
@@ -0,0 +1,11 @@
+class Atm:
+    def __init__(self): #  here __init__ is a constructor. it is a special method that 
+                                # initializes the object when it is created.in python, constructor
+                                # name is always __init__.
+        # self.balance=0
+        # self.pin=""
+
+        print("welcome to the ATM")
+
+NICA=Atm()     #this is object 
+        
